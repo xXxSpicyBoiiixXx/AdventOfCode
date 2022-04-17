c0e0f60ae7ddfc9aec8159272a2fb73ae4116bd0
Md Ali <mali54@hawk.iit.edu>
2022-04-17 16:10:12 -0500

    Avoid brute forcing, trying to find workaround

--- 2021/day-02/main.c
+++ 2021/day-02/main.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <string.h> 
 
 int main() { 
-    
-    int file_counter_size = 0; 
-    char c; // throw away value for now 
+
+    /* Unused variables */
+    //int file_counter_size = 0; 
+    //char c; // throw away value for now 
+    char forward[] = "forward ";
+    char up[] = "up ";
+    char down[] = "down "; 
+
+    uint64_t counter_forward = 0; 
+    uint64_t counter_depth = 0; 
+
+    const unsigned MAX_LENGTH = 256; 
+    char buffer[MAX_LENGTH]; 
 
     FILE *in_file = fopen("input.txt", "r"); 
     FILE *out_file = fopen("answer.txt", "w"); 
 
-    if(in_file = NULL || out_file == NULL) { 
+    if(in_file == NULL || out_file == NULL) { 
         printf("ERROR! Count not open file!\n"); 
         exit(1); 
     }
 
-    for(c = getc(in_file); c != EOF; c = getc(in_file)) { 
-        if(c == '\n') { 
-            file_counter_size++: 
+   // fscanf(in_file, "%s%i", buffer, i); 
+
+    /* Checking if fgets command works */ 
+    while(fgets(buffer, MAX_LENGTH, in_file)) {  
+
+        char buffer_inline[MAX_LENGTH]; 
+        
+       // fscanf(in_file, "%s%d", buffer, i); 
+        for(int i = 0; i < 10; i++) {
+
+         fscanf(in_file, "%s%d", buffer_inline, &i); 
+
+         if(strcmp(buffer, strcat(forward, strcat(i, "\n"))) == 0) { 
+                counter_forward++; 
+            }
+            if(strcmp(buffer, "down i") == 0) { 
+                counter_depth++; 
+            }
+            if(strcmp(buffer, "up i") == 0) { 
+                counter_depth--;
+            }
+
         }
     }
-    
+
+    printf("Horizontal: %llu\n", counter_forward);
+    printf("Depth: %llu\n", counter_depth);  
+
     fclose(in_file); 
     fclose(out_file); 
 
-    printf("The file has %d line", file_counter_size); 
 
 
 }
