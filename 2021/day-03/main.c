63c0dac46e2508068d9df0b1ccde37e7b0dcaf84
Md Ali <spicynotebook@dhcp135.merunorth107.iit.edu>
2022-04-27 14:54:18 -0500

    added edge case

--- 2021/day-03/main.c
+++ 2021/day-03/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
 
 int main() { 
 	
     uint64_t power_consumption = 0;
-    uint64_t gamma_rate = 0;
-    uint64_t epislon_rate = 0;
+    uint64_t gamma_rate = 0; // Most common bit
+    uint64_t epislon_rate = 0; // Least common bit, the inverse of gamma_rate 
+    uint64_t bit_placement = 0;
+    char binary_code; 
 
-    const unsigned MAX_LENGTH = 256; 
+    //const unsigned MAX_LENGTH = 256; 
 
-    char buffer_inline[MAX_LENGTH]; 
+    //char buffer_inline[MAX_LENGTH]; 
 
-    FILE *in_file = fopen("input.txt", r); 
-    FILE *out_file = fopen("answer.txt", w); 
+    FILE *in_file = fopen("input.txt", "r"); 
+    FILE *out_file = fopen("answer.txt", "w"); 
 
     if(in_file == NULL) {
         printf("ERROR! COULD NOT OPEN INPUT FILE!\n"); 
@@ -24,9 +27,15 @@ int main() {
         exit(1); 
     }
 
-    while((fscanf(in_file, "%llu", buffer_inline, &i)) != EOF) {
-        
+    while((binary_code = fgetc(in_file)) != EOF) {
+        //printf("%c", binary_code);
+        if(binary_code != "/n") { 
+            
+        }
     }
+
+    fclose(in_file);
+    fclose(out_file);
 
 
 }
