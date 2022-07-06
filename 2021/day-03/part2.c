b848ef637b52e679eed978789f7b851fff42f10e
xXxSpicyBoiiixXx <mali54@hawk.iit.edu>
2022-07-06 15:54:23 -0500

    Everything but can't skip the none needed numbers

--- 2021/day-03/part2.c
+++ 2021/day-03/part2.c
@@ -6,8 +6,8 @@
 int main() { 
     
     uint64_t life_support = 0; 
-    uint64_t O2_rating[11]; // Oxygen rating
-    uint64_t CO2_rating[11]; // Carbon Dioxide rating
+    uint64_t O2_rating = 0; // Oxygen rating
+    uint64_t CO2_rating = 0; // Carbon Dioxide rating
     
     uint64_t bit_placement = 0; 
     
@@ -22,7 +22,8 @@ int main() {
 
     int binary_code;
     int comparison; 
-    int bit_check;
+    int bit_check_O2;
+    int bit_check_CO2;
 
     FILE *in_file = fopen("input.txt", "r"); 
     FILE *out_file = fopen("answer-part2.txt", "w"); 
@@ -39,7 +40,8 @@ int main() {
 
     for(int i = 0; i < 13; i++) { 
        bit_placement = 0; 
-       bit_check = 0; 
+       bit_check_O2 = 0; 
+       bit_check_CO2 = 0;
        comparison = 0;     
     while((binary_code = fgetc(in_file)) != EOF) { 
         
@@ -53,17 +55,18 @@ int main() {
                   bit_check_CO2++;   
                 }
                 else { 
-                    break; 
+                    // skip the line here
+                    fgets(in_file); 
                 }
             }
-        }
 
-           if(bit_check != comparison) { 
-                break; 
-           } 
+            if((bit_check_O2 + bit_check_CO2) != comparison) { 
+                // skip the line here
+                fgets(in_file); 
+            }
+        } 
 
-         else { 
-
+        if((bit_check_O2 + bit_check_CO2) == comparison) {
         if(binary_code == '1' && bit_placement == i) { 
            one_counter++; 
         }
@@ -78,7 +81,6 @@ int main() {
             bit_placement = 0;
         }
     }
-    
     }
 
     bit_placement = i;
@@ -86,10 +88,10 @@ int main() {
     if(one_counter >= zero_counter) { 
         binary_counter_O2[bit_placement] = 1; 
         binary_counter_CO2[bit_placement] = 0;  
-    } else { 
+    } if(zero_counter > one_counter) { 
         binary_counter_O2[bit_placement] = 0; 
         binary_counter_CO2[bit_placement] = 1; 
-    }
+         }
 
     rewind(in_file);
     one_counter = 0;
@@ -99,8 +101,7 @@ int main() {
     printf("\n");
     printf("The number of zeros in placement one: %llu", zero_counter);
     printf("\n");
-  */
-
+*/
 for(int j = 0; j < 12; j++) { 
     printf("%llu", binary_counter_O2[j]); 
 }
@@ -113,7 +114,31 @@ for(int j = 0; j < 12; j++) {
 
 printf("\n");
 
+for(int j = 11; j >= 0; j--) { 
+    
+    if(binary_counter_O2[j] == 1) { 
+        O2_rating = O2_rating + pow(2, j); 
+    } 
+
+    if(binary_counter_CO2[j] == 1) { 
+        CO2_rating = CO2_rating + pow(2, j);
+    }
+}
+
+printf("O2 Rating: %llu", O2_rating); 
+printf("\n");
+printf("CO2 Rating: %llu", CO2_rating); 
+printf("\n");
+
+life_support = O2_rating * CO2_rating; 
+
+printf("LIFE SUPPORT: %llu", life_support); 
+printf("\n");
+
     fclose(in_file);
     fclose(out_file);
+
+    free(binary_counter_O2);
+    free(binary_counter_CO2); 
 
 }
