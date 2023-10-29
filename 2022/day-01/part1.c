c52fe902ac96bd410e756ac2da91286293021a63
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2023-10-29 16:36:10 -0500

    Confusion

--- 2022/day-01/part1.c
+++ 2022/day-01/part1.c
@@ -1,28 +1,42 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h> 
+#include <stdint.h>
 
 int main() { 
     
-    int counter = 0; 
+   // uint64_t counter = 0;     
+   // uint64_t max_weight = 0;
+   // uint64_t temp_weight = 0;
+    uint64_t file_values = 0; 
     
-    int max_weight = 0;
-    int temp_weight = 0; 
-    
+    struct stat sb; 
+
     FILE *in_file = fopen("input.txt", "r"); 
     FILE *out_file = fopen("answer-part1.txt", "w"); 
     
     if(in_file == NULL || out_file == NULL) { 
         printf("ERROR! Count not open file!\n"); 
         exit(-1);
+    } 
+
+    if(stat("input.txt", &sb) == -1) { 
+        printf("ERROR! Stat");
+        exit(-1);
+    } else {
+        char *buf = malloc(sb.st_size+1);
+
+        if(buf == NULL) { 
+            printf("NOT ENOUGH MEM"); 
+            exit(-1);
+        }
+    
     }
-
     
-    
-    fprintf(out_file, "The strongest boi can lift: %d", max_weight); 
-    printf("The strongest boi can life: %d", max_weight); 
-    printf("\nThis is written in answer-part1.txt"); 
-
     fclose(in_file); 
     fclose(out_file);
 
