1017b8e26cb253b524eb49a8dbc52d6fdd6ca7ee
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2023-10-29 15:28:34 -0500

    Start 2022

--- 2022/day-01/part1.c
+++ 2022/day-01/part1.c
@@ -12,28 +12,13 @@ int main() {
     FILE *in_file = fopen("input.txt", "r"); 
     FILE *out_file = fopen("answer-part1.txt", "w"); 
     
-    char ch;
-    
     if(in_file == NULL || out_file == NULL) { 
         printf("ERROR! Count not open file!\n"); 
         exit(-1);
     }
 
     
-    do { 
-        ch = fgetc(in_file);
-        //printf("%c", ch); 
-        if(strcpy(&ch, NULL) == 0 && max_weight < temp_weight) { 
-            max_weight = temp_weight; 
-            counter = 0; 
-            temp_weight = 0;
-        }
-
-        counter = atoi(&ch); 
-        temp_weight = temp_weight + counter; 
-
-    } while(ch != EOF); 
-   
+    
     fprintf(out_file, "The strongest boi can lift: %d", max_weight); 
     printf("The strongest boi can life: %d", max_weight); 
     printf("\nThis is written in answer-part1.txt"); 
