d90f0ace146e2299d241e8f01d42cc3f13d1641a
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2023-11-03 18:27:59 -0500

    Why

--- 2022/day-01/part1.c
+++ 2022/day-01/part1.c
@@ -9,12 +9,16 @@
 
 int main() { 
     
+    const unsigned MAX_LENGTH = 256; 
+
+    char buffer_inline[MAX_LENGTH]; 
+    int c[1000];
+
    // uint64_t counter = 0;     
    // uint64_t max_weight = 0;
    // uint64_t temp_weight = 0;
     uint64_t file_values = 0; 
-    
-    struct stat sb; 
+    uint64_t data[2000];
 
     FILE *in_file = fopen("input.txt", "r"); 
     FILE *out_file = fopen("answer-part1.txt", "w"); 
@@ -23,22 +27,13 @@ int main() {
         printf("ERROR! Count not open file!\n"); 
         exit(-1);
     } 
-
-    if(stat("input.txt", &sb) == -1) { 
-        printf("ERROR! Stat");
-        exit(-1);
-    } else {
-        char *buf = malloc(sb.st_size+1);
-
-        if(buf == NULL) { 
-            printf("NOT ENOUGH MEM"); 
-            exit(-1);
-        }
     
-    }
+    fscanf(in_file, "%[^\n]", c);
+    printf("Data from the file:\n%i", c); 
     
     fclose(in_file); 
     fclose(out_file);
-
+    
     return 0;
+   
 }
