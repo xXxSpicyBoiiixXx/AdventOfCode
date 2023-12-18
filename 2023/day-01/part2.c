6593d7ab3853825bec0f890dd318dd3f08bf7ce8
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2023-12-18 08:57:28 -0600

    get back to part 2 later

--- 2023/day-01/part2.c
+++ 2023/day-01/part2.c
@@ -12,49 +12,75 @@
 
 // Function to extract the calibration value from a line
 int extractValue(const char *line) {
-    int firstDigit = -1;
-    int lastDigit = -1; 
+    
+    char *digitWords[] = {"one", "two", "three", "four", "five",
+                        "six", "seven", "eight", "nine"};
+    char digits[10];
+    //char firstDigit;
+    //char lastDigit;
+    int count = 0; 
 
-    // Finding the first digit
     for(int i = 0; line[i] != '\0'; i++) { 
-        if(isdigit(line[i])) {
-            firstDigit = line[i] - '0';
-            break;
+        if(isdigit(line[i]) || isalpha(line[i])) {
+            digits[count++] = line[i];
+
+            //Adding this here to handle buffer overflow errors
+            if(count == sizeof(digits) - 1) { 
+                digits[count] = '\0';
+                break;
+            }
+        } else { 
+            digits[count] = '\0';
+            count = 0;
+
+            for(int j = 0; j < 9; j++) { 
+                if(strcmp(digits, digitWords[j]) == 0) {
+                    digits[0] = '0' + (j + 1); 
+                    count = 1; 
+                    break;
+                }
+            }
         }
     }
 
-    // Finding the last digit
-    for(int i = 0; line[i] != '\0'; i++) { 
-        if(isdigit(line[i])) { 
-            lastDigit = line[i] - '0';
-        }
+    // Combination of the first and last digits
+    // Returns that value in the return statement
+    /*firstDigit = digits[0];
+    lastDigit = digits[count - 1];
+    */
+    return (digits[0] - '0') * 10 + (digits[count - 1] - '0');
+}
+
+int correctedValue(FILE *inputFile) {
+    
+    int totalSum = 0;
+    char line[1000];
+
+    while(fgets(line, sizeof(line), inputFile) != NULL) { 
+        totalSum += extractValue(line);
     }
 
-    // Combinig the first and last digit
-    int calibrationValue = firstDigit * 10 + lastDigit;
-    return calibrationValue;
+    return totalSum;
 }
 
 int main() {
-    char line[1000];
-    int totalCalibrationValue = 0;
+    
+    int totalCalibrationValue;
     FILE *in_file = fopen("input.txt", "r");
     FILE *out_file = fopen("answers.txt", "w");
-
+    
     if(in_file == NULL || out_file == NULL) { 
         printf("ERROR! Could not open file!\n");
         exit(-1);
     }
 
-    while(fgets(line, sizeof(line), in_file) != NULL) {
-        totalCalibrationValue += extractValue(line);    
-    }
+    totalCalibrationValue = correctedValue(in_file); 
 
-    printf("Total Calibration Value: %d\n", totalCalibrationValue); 
+    printf("Corrected Calibration Value: %d\n", totalCalibrationValue); 
     printf("Your results have been save to answers.txt\n\n");
-    fprintf(out_file, "Part 1 Answer\n"); 
+    fprintf(out_file, "Part 2 Answer\n"); 
     fprintf(out_file, "-------------\n");
-    fprintf(out_file, "Total Calibration Value: %d\n", totalCalibrationValue);
+    fprintf(out_file, "Corrected Calibration Value: %d\n", totalCalibrationValue);
     fprintf(out_file, "-------------\n\n");      
 
     fclose(in_file);
