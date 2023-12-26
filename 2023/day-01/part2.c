5bf5f601d363bf94c5a676964f809ac0e5030eae
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2023-12-25 18:10:58 -0600

    errors

--- 2023/day-01/part2.c
+++ 2023/day-01/part2.c
@@ -10,20 +10,48 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_LINE_LENGTH 100
+
 // Function to extract the calibration value from a line
 int extractValue(const char *line) {
+    int length = strlen(line); 
     int firstDigit = -1; 
     int lastDigit = -1; 
+    
+    for(int i = 0; i < length; i++) { 
+        char currentInput = line[i];
 
-    char *digitWords[] = {"one", "two", "three", "four", "five",
-                        "six", "seven", "eight", "nine"};
-    char digits[10];
+        if(currentInput >= '0' && currentInput <= '9') { 
+            if(firstDigit == -1) { 
+                firstDigit = currentInput - '0'; 
+            } lastDigit = currentInput - '0';
+        } else if(currentInput >= 'a' && currentInput <= 'z') { 
+            // Converting digits to alpha
+            int digitValue = currentInput - 'a' + 1; 
+
+            if(firstDigit == -1) { 
+                firstDigit = digitValue;
+            } lastDigit = digitValue; 
+        }
+    }
+
+    if(firstDigit == -1 || lastDigit == -1) {
+        fprintf(stderr, "Invalid input at line: %s\n", line);
+        exit(EXIT_FAILURE);
+    }
+
+    return firstDigit * 10 + lastDigit; 
+
+}
+    //char *digitWords[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+ //   char digits[10];
     //char firstDigit;
     //char lastDigit;
-    int count = 0; 
+   // int count = 0; 
     
     // Catching the first digit
-    for(int i = 0; line[i] != '\0'; i++) { 
+   /* for(int i = 0; line[i] != '\0'; i++) { 
+
         if(isdigit(line[i])) { 
             firstDigit = line[i] - '0'; 
             break;
@@ -33,7 +61,7 @@ int extractValue(const char *line) {
         } else {
             printf("ERROR WITH FIRST DIGIT");
         }
-    }
+    }*/
 /*
     // Catching the last digit
     for(int i = 0; line[i] != '\0'; i++) { 
@@ -68,7 +96,7 @@ int extractValue(const char *line) {
     /*firstDigit = digits[0];
     lastDigit = digits[count - 1];
     */
-
+/*
     printf("%d\n",(digits[0] - '0' ) * 10 + digits[count - 1] - '0');
      
     return (digits[0] - '0') * 10 + (digits[count - 1] - '0');
@@ -84,11 +112,11 @@ int correctedValue(FILE *inputFile) {
     }
 
     return totalSum;
-}
+}*/
 
 int main() {
     
-    int totalCalibrationValue;
+    //int totalCalibrationValue;
     FILE *in_file = fopen("input.txt", "r");
     FILE *out_file = fopen("answers.txt", "w");
     
@@ -97,7 +125,18 @@ int main() {
         exit(-1);
     }
 
-    totalCalibrationValue = correctedValue(in_file); 
+    int totalCalibrationValue = 0;
+    int calibrationValue = 0;
+    char line[MAX_LINE_LENGTH];
+
+
+    while(fgets(line, sizeof(line), in_file) != NULL) { 
+        line[strcspn(line, "\n")] = '\0';
+        calibrationValue = extractValue;
+        totalCalibrationValue += calibrationValue;
+    }
+
+    //totalCalibrationValue = correctedValue(in_file); 
 
     printf("Corrected Calibration Value: %d\n", totalCalibrationValue); 
     printf("Your results have been save to answers.txt\n\n");
