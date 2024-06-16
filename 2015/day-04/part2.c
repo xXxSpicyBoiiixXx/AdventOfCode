37c347a8361e8ff2a2f8764dca98c4295cd1c958
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2024-06-15 20:35:39 -0500

    day 4 template

---
+++ 2015/day-04/part2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { 
+
+    // declaring file input/output
+    FILE *in_file = fopen("input.txt", "r"); 
+    FILE *out_file = fopen("answer-part2.txt", "w"); 
+
+    // error checking for files
+    if(in_file == NULL || out_file == NULL) { 
+        printf("Error! Count not open file!\n"); 
+        exit(-1);             
+    }
+
+    // closing files
+    fclose(in_file);
+    fclose(out_file); 
+}
+
