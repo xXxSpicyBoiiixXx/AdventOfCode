6a2830e3cf7187860d6cff70b67a8e5e7ee09f76
xXxSpicyBoiiixXx <aliev.mordecai@gmail.com>
2025-01-12 02:28:11 -0600

    Compelted day1

---
+++ 2024/day-01/part1.c
@@ -0,0 +1,55 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int comp(const void* a, const void* b) 
+{ 
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+
+    int col1 = 0;
+    int col2 = 0;
+
+    int arr1[1000];
+    int arr2[1000]; 
+    
+    int i = 0;
+    int totalDistance = 0;
+
+    int bufferLength = 1024;
+    char buffer[bufferLength];
+
+    FILE *in_file = fopen("input.txt", "r");
+    FILE *out_file = fopen("answers-1.txt", "w");
+
+    if(in_file == NULL || out_file == NULL) { 
+        printf("ERROR! Could not open file!\n");
+        exit(-1);
+    }
+    
+    while(fgets(buffer, bufferLength, in_file)) { 
+        if(2==sscanf(buffer, "%i %i", &col1, &col2)) { 
+            //printf("a: %i   b: %i\b", col1, col2);
+            arr1[i] = col1;
+            arr2[i] = col2;
+            i++;        
+        }
+    }
+
+    qsort(arr1, 1000, sizeof(int), comp);
+    qsort(arr2, 1000, sizeof(int), comp);
+
+    for(int i = 0; i < 1000; i++) { 
+        totalDistance += abs(arr1[i] - arr2[i]);
+    }
+    
+    printf("Total distance %i: ", totalDistance);
+    fprintf(out_file, "Total distance %i: ", totalDistance);
+
+    fclose(in_file);
+    fclose(out_file);
+
+    return 0;
+}
---
+++ 2024/day-01/part2.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int comp(const void* a, const void* b) 
+{ 
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+
+    int col1 = 0;
+    int col2 = 0;
+
+    int arr1[1000];
+    int arr2[1000]; 
+    
+    int i = 0;
+    int similarityScore = 0;
+    int multipler = 0;
+
+    int bufferLength = 1024;
+    char buffer[bufferLength];
+
+    FILE *in_file = fopen("input.txt", "r");
+    FILE *out_file = fopen("answers-1.txt", "w");
+
+    if(in_file == NULL || out_file == NULL) { 
+        printf("ERROR! Could not open file!\n");
+        exit(-1);
+    }
+    
+    while(fgets(buffer, bufferLength, in_file)) { 
+        if(2==sscanf(buffer, "%i %i", &col1, &col2)) { 
+            //printf("a: %i   b: %i\b", col1, col2);
+            arr1[i] = col1;
+            arr2[i] = col2;
+            i++;        
+        }
+    }
+    
+    for(int i = 0; i < 1000; i++) { 
+        for(int j = 0; j < 1000; j++) { 
+            if(arr1[i] == arr2[j]) { 
+                multipler++;            
+            }
+        }
+        similarityScore += arr1[i] * multipler;
+        multipler = 0;
+    }
+
+    printf("Similarity Score: %i ", similarityScore); 
+    fprintf(out_file, "Similarity Score: %i ", similarityScore);
+
+
+/*
+    qsort(arr1, 1000, sizeof(int), comp);
+    qsort(arr2, 1000, sizeof(int), comp);
+
+    for(int i = 0; i < 1000; i++) { 
+        totalDistance += abs(arr1[i] - arr2[i]);
+    }
+    
+    printf("Total distance %i: ", totalDistance);
+    fprintf(out_file, "Total distance %i: ", totalDistance);
+*/
+    
+
+
+    fclose(in_file);
+    fclose(out_file);
+
+    return 0;
+}
